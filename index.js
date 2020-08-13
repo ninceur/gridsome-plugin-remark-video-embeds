0071a70c6f47eef2acbcc912d9ec5ef1f6136836
ninceur <sean@muimedia.com>
2020-08-13 09:47:48 -0400

    Implemented the Gatsby image alt tag style of embedding. Works as expected on test site.

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const visit = require('unist-util-visit');
 
 module.exports = (options) => {
-	function getVideoID(userInput) {
-		var res = userInput.match(/^.*(?:(?:youtu.be\/)|(?:v\/)|(?:\/u\/\w\/)|(?:embed\/)|(?:watch\?))\??v?=?([^#\&\?]*).*/);
-		if (res) return res[1];
-		return false;
-	}
 
-  return async tree => {
-		visit(tree, 'inlineCode', node => {
-			if (node.value.startsWith('youtube:')) {
-        const videoID = getVideoID(node.value.slice(8));
-				if (videoID) {
-					node.type = 'html';
-					node.value = `<div style="width: ${options.width || '100%'}; margin: 0 ${options.align || '0'};"><div style="position: relative; padding-bottom: 56.25%; padding-top: 25px; height: 0;"><iframe style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://www.youtube.com/embed/${videoID}"></iframe></div></div>`;
-				}
+  const debug = options.debug ? console.log : () => {};
+
+	return async tree => {
+		// TODO: implement options
+		visit(tree, 'image', node => {
+			const { alt = '', url = '' } = node;
+			console.log("Alt is: " + alt + " and ID is: " + url);
+			// Check alt tag for either Vimeo or Youtube and update embed based on result
+			if (alt === 'youtube') {
+				node.type = 'html';
+				node.value = `<div class="embed video-player" id="video_container" itemprop="video" itemscope itemtype="http://schema.org/VideoObject">
+        <iframe
+            src="https://youtube.com/embed/${url}"
+            class="video-youtube" type="text/html"
+            width="640" height="385"
+            allowfullscreen
+            frameborder="0"
+            >
+    </iframe>
+    </div>`;
 			}
+		else if (alt === 'vimeo') {
+			node.type = 'html';
+			node.value = `<div class="embed video-player" itemprop="video" itemscope itemtype="http://schema.org/VideoObject">
+			<iframe  
+			src="https://player.vimeo.com/video/${url}"
+			class="video-vimeo"
+			width="640" height="385" 
+			frameborder="0" 
+			webkitAllowFullScreen mozallowfullscreen allowFullScreen
+			>
+			</iframe>
+		</div>`;
+		}
 		});
   };
 };
