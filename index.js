9cb08e0881af1c94259964eb650ba7d176d6dfb5
Sean Edevane <sean@muimedia.com>
2020-08-28 23:40:41 -0400

    Updated version number to better reflect development status. Added a style= variable wfor handling aspect ratio scaling on intrinsicly sized elements.

--- index.js
+++ index.js
@@ -8,33 +8,31 @@ module.exports = (options) => {
 		// TODO: implement options
 		visit(tree, 'image', node => {
 			const { alt = '', url = '' } = node;
-			console.log("Alt is: " + alt + " and ID is: " + url);
-			// Check alt tag for either Vimeo or Youtube and update embed based on result
 			if (alt === 'youtube') {
 				node.type = 'html';
-				node.value = `<div class="embed video-player" id="video_container" itemprop="video" itemscope itemtype="http://schema.org/VideoObject">
-        <iframe
-            src="https://youtube.com/embed/${url}"
-            class="video-youtube" type="text/html"
-            width="640" height="385"
-            allowfullscreen
-            frameborder="0"
-            >
-    </iframe>
-    </div>`;
+				node.value = `<div class="embed video-player" style="--aspect-ratio: 9 / 16;" itemprop="video" itemscope itemtype="http://schema.org/VideoObject">
+					<iframe
+						src="https://youtube.com/embed/${url}"
+						class="video-youtube" type="text/html"
+						width="640" height="385"
+						allowfullscreen
+						frameborder="0"
+						>
+					</iframe>
+    			</div>`;
 			}
 		else if (alt === 'vimeo') {
 			node.type = 'html';
-			node.value = `<div class="embed video-player" itemprop="video" itemscope itemtype="http://schema.org/VideoObject">
-			<iframe  
-			src="https://player.vimeo.com/video/${url}"
-			class="video-vimeo"
-			width="640" height="385" 
-			frameborder="0" 
-			webkitAllowFullScreen mozallowfullscreen allowFullScreen
-			>
-			</iframe>
-		</div>`;
+			node.value = `<div class="embed video-player" style="--aspect-ratio: 9 / 16;" itemprop="video" itemscope itemtype="http://schema.org/VideoObject">
+				<iframe  
+				src="https://player.vimeo.com/video/${url}"
+				class="video-vimeo"
+				width="640" height="385" 
+				frameborder="0" 
+				webkitAllowFullScreen mozallowfullscreen allowFullScreen
+				>
+				</iframe>
+			</div>`;
 		}
 		});
   };
